c63211f64d72615c8fdfa13d43e39f53f51b4d5e
Vinicius Tonon <viniciustonon@gmail.com>
2013-10-21 11:22:03 -0200

    Initial Commit

---
+++ ihCarona/ihCarona/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ihCarona
+//
+//  Created by Vinicius Tonon on 10/21/13.
+//  Copyright (c) 2013 Vinicius Tonon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ihCarona/ihCarona/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ihCarona
+//
+//  Created by Vinicius Tonon on 10/21/13.
+//  Copyright (c) 2013 Vinicius Tonon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
