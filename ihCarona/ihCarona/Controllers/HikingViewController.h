d09628e24c68ff9cccdc6f16c462d75ffd4d0c6b
Ero-Radio <pfreme@Pedros-MacBook-Pro-2.local>
2013-10-31 09:06:06 -0200

    Hiking ViewControler created

---
+++ ihCarona/ihCarona/Controllers/HikingViewController.h
@@ -0,0 +1,13 @@
+//
+//  HikingViewController.h
+//  ihCarona
+//
+//  Created by Pedro Freme on 10/31/13.
+//  Copyright (c) 2013 Vinicius Tonon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HikingViewController : UIViewController
+
+@end
---
+++ ihCarona/ihCarona/Controllers/HikingViewController.m
@@ -0,0 +1,42 @@
+//
+//  HikingViewController.m
+//  ihCarona
+//
+//  Created by Pedro Freme on 10/31/13.
+//  Copyright (c) 2013 Vinicius Tonon. All rights reserved.
+//
+
+#import "HikingViewController.h"
+
+@interface HikingViewController ()
+
+@property(nonatomic, strong) NSString *destination;
+@property(nonatomic, strong) NSString *actualLocation;
+@property(nonatomic, strong) NSString *dateToGo;
+
+@end
+
+@implementation HikingViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
