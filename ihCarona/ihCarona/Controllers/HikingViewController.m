5815b4eb01842136c4000efea17047bba53a5d33
Vinicius Tonon <viniciustonon@gmail.com>
2013-10-31 10:27:57 -0200

    JSON Add

--- ihCarona/ihCarona/Controllers/HikingViewController.m
+++ ihCarona/ihCarona/Controllers/HikingViewController.m
@@ -7,12 +7,17 @@
 //
 
 #import "HikingViewController.h"
+#import <MapKit/MapKit.h>
 
 @interface HikingViewController ()
 
 @property(nonatomic, strong) NSString *destination;
 @property(nonatomic, strong) NSString *actualLocation;
 @property(nonatomic, strong) NSString *dateToGo;
+
+@property(nonatomic, strong) NSDictionary *json;
+@property(nonatomic) CLLocationCoordinate2D userLocat;
+
 
 @end
 
@@ -31,6 +36,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view.
+    [self jsonTestMethod];
     UIAlertView *alert = [[UIAlertView alloc]initWithTitle:@"Destino" message:@"Qual seu destino" delegate:nil cancelButtonTitle:@"Wololo" otherButtonTitles:nil];
     
     alert.alertViewStyle = UIAlertViewStylePlainTextInput;
@@ -41,6 +47,30 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Map Data
+- (void)mapView:(MKMapView *)mapView
+didUpdateUserLocation:
+(MKUserLocation *)userLocation
+{
+    userLocation.location.coordinate;
+    self.userLocat = userLocation.location.coordinate;
+}
+
+#pragma mark - JSON
+
+-(void)jsonTestMethod
+{
+    NSString *url = [NSString stringWithFormat:@"http://s3.amazonaws.com/ihCarona/testData.json"];
+    NSData *jsonData = [NSData dataWithContentsOfURL:[NSURL URLWithString:url]];
+    
+    NSDictionary *retornoJSON = [NSJSONSerialization JSONObjectWithData:jsonData options:NSJSONReadingMutableContainers error:nil];
+    
+    NSString *nome = [retornoJSON objectForKey:@"nome"];
+    NSString *endereco = [retornoJSON objectForKey:@"descricao"];
+    
+    NSLog(@"Nome: %@, Endereço: %@", nome, endereco);
 }
 
 #pragma mark - UIAlertViewDelegate
